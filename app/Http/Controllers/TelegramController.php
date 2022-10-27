0766df027b945056bc8febe53d2734a8f170719d
olekor1986 <olekor1986@gmail.com>
2022-10-27 12:17:18 +0100

    modify TelegramController

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,14 +9,32 @@ use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
+    protected $buttons = [
+            'inline_keyboard' => [
+                [
+                    [
+                        'text' => 'Button1',
+                        'callback_data' => '1'
+                    ],
+                    [
+                        'text' => 'Button2',
+                        'callback_data' => '2'
+                    ],
+                    [
+                        'text' => 'Help',
+                        'callback_data' => '/help'
+                    ]
+                ]
+            ]
+        ];
 
     public function getData(Request $request){
+        $keyboard = json_encode($this->buttons);
+        $telegram = new Telegram;
         if (isset($request['message'])){
-            $telegram = new Telegram;
             $inMsg = $request['message'];
-            if ($inMsg['text'] = '/start'){
+            if ($inMsg['text'] == '/start'){
                 $telegramUser = TelegramUser::find($inMsg['from']['id']);
-                Log::info(print_r($telegramUser, true));
                 if($telegramUser === NULL){
                     $newTelegramUserData['telegram_id'] = $inMsg['from']['id'];
                     $newTelegramUserData['is_bot'] = $inMsg['from']['is_bot'];
@@ -32,18 +50,32 @@ class TelegramController extends Controller
 
                     $telegram->request('sendMessage', [
                         'chat_id' => $newTelegramUser->telegram_id,
-                        'text' => $outMsg
+                        'text' => $outMsg,
+                        'parse mode' => 'html',
+                        'reply_markup' => $keyboard
                     ]);
 
                 } else {
                     $outMsg = 'Hello ' . $telegramUser->first_name . '! ';
                     $telegram->request('sendMessage', [
                         'chat_id' => $telegramUser->telegram_id,
-                        'text' => $outMsg
+                        'text' => $outMsg,
+                        'parse mode' => 'html',
+                        'reply_markup' => $keyboard
                     ]);
                 }
             }
-
+        } else if (isset($request['callback_query'])){
+            $callback = $request['callback_query'];
+            if ($callback['data'] == '/help'){
+                $outMsg = 'If a menu button other than MenuButtonDefault is set for a private chat, then it is applied in the chat. Otherwise the default menu button is applied. By default, the menu button opens the list of bot commands.';
+                $telegram->request('sendMessage', [
+                    'chat_id' => $callback['from']['id'],
+                    'text' => $outMsg,
+                    'parse mode' => 'html',
+                    'reply_markup' => $keyboard
+                ]);
+            }
         }
 
     }
